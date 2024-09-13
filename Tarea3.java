d6330c2c07f3a17cdf7970ebbda2beed476a7220
Raul09dev <baezrulo57@gmail.com>
2024-09-12 18:23:42 -0600

    Update Tarea3.java

---
+++ Tarea3.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+/*
+    SECCION 1: LIBRO FAV
+     */
+
+        System.out.println("las batallas en el desierto");
+        System.out.println("Jose Emilio Pacheco");
+        System.out.println(1981);
+        System.out.println("Ficcion literaria");
+/*
+SECCION 2:PAIS FAV
+
+ */
+        String Pais = "españa";
+        String capital = "madrid";
+        String idioma = "españa";
+        double poblacion = 6.500000000;
+        String Monesa = "euro";
+
+        System.out.println(Pais);
+        System.out.println(capital);
+        System.out.println(idioma);
+        System.out.println(poblacion);
+        System.out.println(Monesa);
+/*
+SECCION 3: PERSONAJE FAV
+ */
+        String Nombre = "Magdalena Carmen Kahlo Calderon";
+        String fecha = "6 de julio de 1907";
+        String nacionalidad = "Mexicana";
+        String ocupacion = "pintora";
+        String logrp = "Autorretratos";
+
+
+        System.out.println(Nombre);
+        System.out.println(fecha);
+        System.out.println(nacionalidad);
+        System.out.println(ocupacion);
+        System.out.println(logrp);
+
+        }
+    }
