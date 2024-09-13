6b0d9059df9891404fc13e0e481e4b32a48772c5
Raul09dev <baezrulo57@gmail.com>
2024-09-12 22:19:51 -0600

    Update tarea4.java

---
+++ tarea4.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+ double resultado = ((12 + 8)*(5 - 3)-( 6/ 2)) / ((3 * 4)+(8 / 2));
+ System.out.println(resultado);
+
+   /*
+   EJERCISIO 3
+    */
+        double formula2 = (((9 * 5) - (15 + 3)) + ((8 * 2) - (12 / 4.0)) * (7 + 2) - ((6 * 3) / 2.0))
+                / (2 + ((3 * 2) / (4 - 2.0)) + (5 * ((8 / 4.0) + 1)) - (6 / (2.0 + 1)));
+   System.out.println(formula2);
+   /*
+   Eerision 3
+    */
+        double resultado3 = (((6 + 4 )* 3) - (18 /2 )) /((8 - 5)+(3 * 2));
+        System.out.println(resultado3);
+    }
+}
