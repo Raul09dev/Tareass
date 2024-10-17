fd89e1f17ae4a7843d85d943a17a2dbfa3025c2f
Raul09dev <baezrulo57@gmail.com>
2024-10-16 22:55:48 -0600

    Update tarea3-unidad3.java

---
+++ tarea3-unidad3.java
@@ -0,0 +1,141 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       if (3<5 && 7>= 2) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+        if (10 != 8 || 15 == 15) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+
+        if (!(5>6) && 2<= 3) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+        if (8==8 ||  3<1 && 6> 2 ) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+        if (12 != 10 && (4<8 || 9==9)) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+        if ((3+5)==8 || (7*2) <= 14) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+
+        if (!(9<=5) && (4==4 || 6 !=6)) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+
+        if (10 % 2 == 0 && (5 > 1 || 12 != 10)) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+        //9
+
+        if (3*3 == 9 && !(8<5 || 7>=9))  {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+        if (!(5+5 != 10 && 2*3>5 || 6 ==7)) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+        if (20/4 == 5 || !(10%3==1 && 4>=3)) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+
+        if (!(7>3 && 2==2 || 5*2 < 12 && 8>=8)) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+
+        if (!(15 -5 >8) && !(3==3 || 7<1) && 12>10) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+
+        if (9%3 == 0 && 8<=12 || 5+5 !=10 && !(7>8)) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+
+        if (!(10 !=10) && (5*3== 15 || 2<1) && 9>= 9) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+
+
+        if (4+6 == 10 || 2 == 3 && 5*3 == 15 || 2<1 && 9 >=9) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+
+
+        if (!(10 != 10) && (8<=15 || 2<1)&& 9>=9) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+
+
+        if (12 / 3 >4 && 5==5 || 10!=10 && !(8>=9)) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+
+
+        if (7 + 3 < 10 && 9 / 3 == 3|| 6 < 2 && 5 * 4 >= 20 ) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+
+
+        if (((10 - 2) != 8 && 6 >= 7 || 2 == 2) && 12 % 3 == 0) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+
+        if (!(15 >5) && 3+2 == 5 || 9<10 && !(6<=4 && 7*2 != 14)) {
+            System.out.println("veradera");
+        } else { System.out.println("Falsa");
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
